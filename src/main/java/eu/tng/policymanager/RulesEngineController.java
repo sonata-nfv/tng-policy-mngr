a537a38f27fe79330aac40db26828071b74fe603
efotopoulou <fwtopoulou@gmail.com>
2018-05-10 11:22:02 +0300

    try to post a policy descriptor

--- src/main/java/eu/tng/policymanager/RulesEngineController.java
+++ src/main/java/eu/tng/policymanager/RulesEngineController.java
@@ -6,6 +6,7 @@ import com.sun.jersey.api.client.WebResource;
 import eu.tng.policymanager.response.BasicResponseCode;
 import eu.tng.policymanager.response.PolicyRestResponse;
 import eu.tng.policymanager.transferobjects.MonitoringMessageTO;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -65,23 +66,51 @@ public class RulesEngineController {
 
     @RequestMapping(value = "", method = RequestMethod.POST)
     public PolicyRestResponse savePolicyDescriptor(@RequestBody String tobject) {
-        String output;
-            //save locally
-        //rulesEngineService.savePolicyDescriptor(tobject);
+        String output = "";
+        //save locally
+        rulesEngineService.savePolicyDescriptor(tobject);
         //save to catalogues
-        // RestTemplate restTemplate = new RestTemplate();
-        // HttpHeaders httpHeaders = new HttpHeaders();
-        // httpHeaders.set("Content-Type", "application/json");
-        // httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-        // httpHeaders.add("Accept", MediaType.APPLICATION_JSON.toString());
+        log.info("i call catalogues with spring rest template");
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("Content-Type", "application/json");
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        httpHeaders.add("Accept", MediaType.APPLICATION_JSON.toString());
 
-        //HttpEntity<String> httpEntity = new HttpEntity<>(tobject, httpHeaders);
-//
-//        log.info(httpEntity.getHeaders().toString());
-//        log.info("policies_url" + policies_url);
-        //String response = restTemplate.postForObject(policies_url, httpEntity, String.class);
-        // ResponseEntity response = restTemplate.exchange(policies_url, HttpMethod.POST, httpEntity, String.class);
-        // ResponseEntity<String> response = restTemplate.postForEntity(policies_url, httpEntity, String.class);
+        HttpEntity<String> httpEntity = new HttpEntity<>(tobject, httpHeaders);
+
+        String responseone = null;
+        try {
+            responseone = restTemplate.postForObject(policies_url, httpEntity, String.class);
+
+            log.info("responseOne" + responseone);
+
+        } catch (Exception e) {
+            log.info("-------------------------------responseOne Exception-------------------------------");
+            e.printStackTrace();
+            //return new PolicyRestResponse(BasicResponseCode.SUCCESS, Message.POLICY_CREATED_FAILURE, "Failed : HTTP error code : " + responseone);
+            //log.info("responseOne" + responseone);
+        }
+        ResponseEntity responseTwo = null;
+        try {
+            responseTwo = restTemplate.exchange(policies_url, HttpMethod.POST, httpEntity, String.class);
+        } catch (Exception e) {
+            log.info("-------------------------------responseTwo Exception-------------------------------");
+            e.printStackTrace();
+            //return new PolicyRestResponse(BasicResponseCode.SUCCESS, Message.POLICY_CREATED_FAILURE, "Failed : HTTP error code : " + responseTwo);
+
+        }
+        ResponseEntity<String> responseThree = null;
+        try {
+            responseThree = restTemplate.postForEntity(policies_url, httpEntity, String.class);
+        } catch (Exception e) {
+            log.info("-------------------------------responseThree Exception-------------------------------");
+            e.printStackTrace();
+            //return new PolicyRestResponse(BasicResponseCode.SUCCESS, Message.POLICY_CREATED_FAILURE, "Failed : HTTP error code : " + responseThree);
+
+        }
+
+        log.info("i call catalogues with basic Jersey library");
         ClientResponse response = null;
         try {
 
@@ -89,6 +118,7 @@ public class RulesEngineController {
 
             WebResource webResource = client.resource(policies_url);
 
+            webResource.accept(MediaType.APPLICATION_JSON_UTF8_VALUE);
             response = webResource.type("application/json").post(ClientResponse.class, tobject);
 
             if (response.getStatus() != 201) {
@@ -101,9 +131,42 @@ public class RulesEngineController {
             System.out.println(output);
 
         } catch (Exception e) {
-            e.printStackTrace();
-            return new PolicyRestResponse(BasicResponseCode.SUCCESS, Message.POLICY_CREATED_FAILURE, "Failed : HTTP error code : "+ response.getStatus());
+            log.info("-------------------------------response with Jersey Exception-------------------------------");
+            log.info("Failed : HTTP error code : " + response.getStatus());
+            //e.printStackTrace();
+            //return new PolicyRestResponse(BasicResponseCode.SUCCESS, Message.POLICY_CREATED_FAILURE, "Failed : HTTP error code : " + response.getStatus());
 
+        }
+        log.info("i call catalogues HttpURLConnection - same way as slas");
+        try {
+
+            URL object = new URL(policies_url);
+
+            HttpURLConnection con = (HttpURLConnection) object.openConnection();
+            con.setDoOutput(true);
+            con.setDoInput(true);
+            con.setRequestProperty("Content-Type", "application/json");
+            con.setRequestProperty("Accept", "application/json");
+            con.setRequestMethod("POST");
+            OutputStreamWriter wr = new OutputStreamWriter(con.getOutputStream());
+            wr.write(tobject);
+            wr.flush();
+
+            StringBuilder sb = new StringBuilder();
+            int HttpResult = con.getResponseCode();
+            if (HttpResult == HttpURLConnection.HTTP_OK) {
+                BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"));
+                String line = null;
+                while ((line = br.readLine()) != null) {
+                    sb.append(line + "\n");
+                }
+                br.close();
+                System.out.println("" + sb.toString());
+            } else {
+                System.out.println(con.getResponseMessage());
+            }
+        } catch (Exception e) {
+            log.info("-------------------------------response with HttpURLConnection Exception-------------------------------");
         }
 
         return new PolicyRestResponse(BasicResponseCode.SUCCESS, Message.POLICY_CREATED, output);
@@ -132,6 +195,16 @@ public class RulesEngineController {
 
         ResponseEntity<String> response = restTemplate.exchange(policies_url, HttpMethod.GET, entity, String.class);
 
+        ///////////////////////////////////Test
+//        String tobject ="{\n  \"name\": \"samplepolicydemo11\",\n  \"descriptor_schema\": \"https://raw.githubusercontent.com/sonata-nfv/tng-schema/master/policy-descriptor/policy-schema.yml\",\n  \"policyRules\": [\n    {\n      \"salience\": 1,\n      \"inertia\": {\n        \"value\": 30,\n        \"duration_unit\": \"m\"\n      },\n      \"name\": \"actionUponAlert\",\n      \"conditions\": {\n        \"condition\": \"AND\",\n        \"rules\": [\n          {\n            \"input\": \"text\",\n            \"field\": \"vnf1.LogMetric\",\n            \"id\": \"vnf1.LogMetric\",\n            \"type\": \"string\",\n            \"value\": \"mon_rule_vm_cpu_perc\",\n            \"operator\": \"equal\"\n          }\n        ]\n      },\n      \"actions\": [\n        {\n          \"action_type\": \"InfrastructureType\",\n          \"name\": \"ApplyFlavour\",\n          \"value\": \"3\",\n          \"action_object\": \"ComponentResourceAllocationAction\",\n          \"target\": \"vnf1\"\n        }\n      ]\n    },\n    {\n      \"salience\": 1,\n      \"duration\": {\n        \"value\": 10,\n        \"duration_unit\": \"m\"\n      },\n      \"inertia\": {\n        \"value\": 30,\n        \"duration_unit\": \"m\"\n      },\n      \"name\": \"highTranscodingRateRule\",\n      \"aggregation\": \"avg\",\n      \"conditions\": {\n        \"condition\": \"AND\",\n        \"rules\": [\n          {\n            \"input\": \"number\",\n            \"field\": \"vnf1.CPULoad\",\n            \"id\": \"vnf1.CPULoad\",\n            \"type\": \"double\",\n            \"value\": \"70\",\n            \"operator\": \"greater\"\n          },\n          {\n            \"input\": \"select\",\n            \"field\": \"vnf2.RAM\",\n            \"id\": \"vnf2.RAM\",\n            \"type\": \"integer\",\n            \"value\": \"8\",\n            \"operator\": \"less\"\n          }\n        ]\n      },\n      \"actions\": [\n        {\n          \"action_type\": \"InfrastructureType\",\n          \"name\": \"ApplyFlavour\",\n          \"value\": \"3\",\n          \"action_object\": \"ComponentResourceAllocationAction\",\n          \"target\": \"vnf1\"\n        }\n      ]\n    }\n  ]\n}";
+//        HttpHeaders headers1 = new HttpHeaders();
+//        headers1.setContentType(MediaType.APPLICATION_JSON);
+//        HttpEntity<String> entity1 = new HttpEntity<>(tobject, headers1);
+//
+//        ResponseEntity<String> response1 = restTemplate.exchange("http://localhost:8081/api/policymngr/v1/policy-descriptor", HttpMethod.POST, entity1, String.class);
+//
+//        log.info(response1.getBody());
+//        
         return response.getBody();
     }
 
