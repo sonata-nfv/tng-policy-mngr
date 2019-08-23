8acd9b3c7a1a4ab4ace99f2604357328dc74d306
efotopoulou <fwtopoulou@gmail.com>
2019-08-23 14:13:22 +0300

    register to NS_TERMINATION_TOPIC (4)

--- src/main/java/eu/tng/policymanager/Messaging/TerminatedNSListener.java
+++ src/main/java/eu/tng/policymanager/Messaging/TerminatedNSListener.java
@@ -6,10 +6,12 @@
 package eu.tng.policymanager.Messaging;
 
 import eu.tng.policymanager.RulesEngineApp;
+import eu.tng.policymanager.RulesEngineService;
 import eu.tng.policymanager.rules.generation.Util;
 import java.nio.charset.StandardCharsets;
 import java.sql.Timestamp;
 import java.util.logging.Level;
+import org.json.JSONObject;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -24,14 +26,21 @@ public class TerminatedNSListener {
     @Autowired
     LogsFormat logsFormat;
 
+    @Autowired
+    RulesEngineService rulesEngineService;
+
     @RabbitListener(queues = RulesEngineApp.NS_TERMINATION_QUEUE)
     public void terminatedNSMessageReceived(byte[] message) {
         Timestamp timestamp = new Timestamp(System.currentTimeMillis());
 
         try {
             String terminatedNSasYaml = new String(message, StandardCharsets.UTF_8);
-            String jsonobject = Util.convertYamlToJson(terminatedNSasYaml);
-            logsFormat.createLogInfo("I", timestamp.toString(), "NS Termination Message received", jsonobject, "200");
+            String jsonobject_as_string = Util.convertYamlToJson(terminatedNSasYaml);
+            logsFormat.createLogInfo("I", timestamp.toString(), "NS Termination Message received", jsonobject_as_string, "200");
+
+            JSONObject jsonobject = new JSONObject(jsonobject_as_string);
+            String ns_uuid = jsonobject.getString("serv_id");
+            rulesEngineService.removeKnowledgebase(ns_uuid);
         } catch (Exception e) {
             logsFormat.createLogInfo("E", timestamp.toString(), "Ignoring message from NS_TERMINATION_QUEUE", e.getMessage(), "200");
         }
